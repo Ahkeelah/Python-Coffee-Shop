1a4f6691996eb4f04d3d1bdc66d72c8f624663a2
Merchant151 <jmiller32@ggc.edu>
2025-06-21 23:46:30 -0400

    landed on an answer decided not to do function wrapping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+options = ['a bundle of foo','a bag of eggs','an option','exit']
 
 def takeSecond(t,numOfBreaks=1):
     time.sleep(t)
@@ -10,19 +11,53 @@ def clearTerm():
         print('\n\n')
 
 def userIn(options):
-    ans = 1
-    for i in options:
-        print(str(ans) + ') '+ str(i))
-        ans +=1
+    while True:
+        print('please select one of the following options: \n')
 
-    x = input()
-    return x
+        ans = 1
+        for i in options:
+            print(str(ans) + ') '+ str(i))
+            ans +=1
+
+        x = input()
+        try: 
+            ans = int(x) -1
+            ans = options[ans]
+        except: 
+            print('uh-oh you made a typo, try again!')
+            takeSecond(0.5)
+            continue
+        break
+    print('\nI see you have selected: '+ ans)
+    return ans
+
+def gameSelector(options,prompt,destinations):
+    def selectorLoop():
+        prompt()
+        ans = userIn(options)
+        destination()
+    return selectorLoop
 
 def intro():
     clearTerm()
     print(' Hello welcome to the Python Coffee shop!')
     print('You will be presented with options type that option to proceed')
     takeSecond(1,2)
+    while True:
+        ans = userIn(options)
+        if (ans == 'exit'): 
+            end()
+            break
+        if (ans == 'a bag of eggs'): 
+            print('smelly stuff')
+            takeSecond(0.5,1)
+
+#intro = gameSelector(options,intro)
+
+def end():
+    print('Goodbye!')
+    exit
+
 
 def testMain():
     intro()
@@ -30,20 +65,16 @@ def testMain():
     while True:
         print('please select one of the following options: \n')
         ans = userIn(options)
-        try: 
-            ans = int(ans) - 1
-            ans = options[ans]
-        except:
-            print('Uh made a typeO try again!')
-            takeSecond(0.5)
-            continue 
+
         #don't think I need this atm
         #print('user picked ans '+ options[(int(ans)-1)])
-        print('\nI see you have selected: '+ ans)
         if (ans == 'exit'):
             print('Goodbye!')
             break
         takeSecond(2)
 
-
-testMain()
+def runGame():
+    intro()
+    end()
+#testMain()
+runGame()
